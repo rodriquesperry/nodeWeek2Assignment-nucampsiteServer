9718f1ef9727aa11b96dd30b7ed9820c02b64f35
Rodriques <rodriques.perry@gmail.com>
2021-04-25 21:01:57 -0500

    Node-MongoDB Week 3 Workshop Assignment Task 3 & 4

--- authenticate.js
+++ authenticate.js
@@ -41,7 +41,7 @@ exports.verifyAdmin = (req, res, next) => {
   if (req.user.admin) {
     return next();
   } else {
-    err = new Error(`You are not authorized to perform this operation!`);
+    const err = new Error(`You are not authorized to perform this operation!`);
     err.status = 403;
     return next(err);
   } 
