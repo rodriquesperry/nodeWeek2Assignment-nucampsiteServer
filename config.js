b3dab311c1f5556bb1b45e603a0d27931a4e06cd
Rodriques <rodriques.perry@gmail.com>
2021-05-09 00:49:39 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey': '12345-67890-09876-54321',
-  'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+  'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+  'facebook': {
+    clientId: '4016637321758201',
+    clientSecret: '867650bde8ba5dec95254ddcda2a9aff'
+  }
 }
